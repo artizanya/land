62e7f63582a0e3380b28cc027ae8b474ec2212a6
ramblehead <v.rybynok@gmail.com>
2019-07-19 17:32:34 +0100

    Checkpoint

--- src/scripts/teardown.ts
+++ src/scripts/teardown.ts
@@ -1,16 +1,22 @@
 // -*- coding: utf-8 -*-:
 
-import { db } from '@arangodb';
 import * as gm from '@arangodb/general-graph';
+import { db } from '@arangodb';
 
 const mxt = module.context;
 
-db._drop(mxt.collectionName('elements'));
-db._drop(mxt.collectionName('components'));
+db._drop(mxt.collectionName('artizans'));
+db._drop(mxt.collectionName('projects'));
 db._drop(mxt.collectionName('processes'));
+db._drop(mxt.collectionName('elements'));
 
-db._drop(mxt.collectionName('componentTypes'));
+db._drop(mxt.collectionName('artizanProjects'));
+db._drop(mxt.collectionName('projectMainProcess'));
+db._drop(mxt.collectionName('projectProcesses'));
+db._drop(mxt.collectionName('projectElements'));
 db._drop(mxt.collectionName('processInputs'));
 db._drop(mxt.collectionName('processOutputs'));
 
+gm._drop(mxt.collectionName('artizansGraph'));
+gm._drop(mxt.collectionName('projectsGraph'));
 gm._drop(mxt.collectionName('processesGraph'));
